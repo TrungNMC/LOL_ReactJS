1930aed0076b94361098309378b39df0522c4fff
NMCT <67629495+NMTC-K59@users.noreply.github.com>
2021-11-02 21:57:03 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.scss'
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/effect-fade'
+
+
+import Header from './components/header/Header'
+import Home from './pages/Home'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Header/>
+      <Home/>
+    </div>
+  )
+}
+
+export default App
